79a33bdde65eedf097ddcdc41197a577688d02ba
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-08-10 12:43:31 +0530

    This is a predefine multiplication

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+class Multiplication{
+    public static void main(String ...args)
+    {
+        System.out.println("Enter two numbers :");
+        int a = 7; //initialize 1st local variable
+        int b = 5; //initialize 2nd local variable
+        System.out.println(a+" "+b); // 7 5
+        int mul = a*b; //logic
+        System.out.println("The multiplication is :"+mul);
+    }
+}
