0edc0507d8d6b8298b382f93808d26d283c2a27d
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-08-10 12:39:35 +0530

    This is a predefine subtraction

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+class Subtraction{
+    public static void main(String ...args)
+    {
+        System.out.println("Enter two numbers :");
+        int a = 100; //initialize 1st local variable
+        int b = 50; //initialize 2nd local variable
+        System.out.println(a+" "+b); // 100 50
+        int sub = a-b; //logic
+        System.out.println("The sub is :"+sub);
+    }
+}
