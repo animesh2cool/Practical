d81c77d87d489fe74e8182e18ac03038ec456ea7
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-08-10 12:34:33 +0530

    this is a predefine addition

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Addition{
+    public static void main(String ...args)
+    {
+        System.out.println("Enter two numbers :");
+        int a = 5; //initialize 1st local variable
+        int b = 9; //initialize 2nd local variable
+        System.out.println(a+" "+b); // 5 6
+        int sum = a+b; //logic
+        System.out.println("The sum is :"+sum);
+    }
+}
